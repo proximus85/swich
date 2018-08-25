5ef204018fe2e7798f8589c43df44da18d07c73d
pawelj <allmightypol@gmail.com>
2018-08-25 23:14:39 +0200

    Initial commit

---
+++ src/main/java/StatementPrinter.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Vector;
+
+public class StatementPrinter {
+
+
+    public static void main(String[] args) {
+        StatementPrinter statementPrinter = new StatementPrinter();
+
+        Customer someCustomer = new Customer("Patrick Thompson");
+
+        Collection<Movie> movies = createMovies();
+        Vector<Rental> rentals = createRentals(movies);
+        rentals.stream().forEach(r -> someCustomer.addRental(r));
+
+        System.out.println(someCustomer.statement());
+    }
+
+    private static Collection<Movie> createMovies() {
+        Movie movieOne = new Movie("Terminator", Movie.NEW_RELEASE);
+        Movie movieTwo = new Movie("Chip and Dale", Movie.CHILDRENS);
+        Movie movieThree = new Movie("Tango and Cash", Movie.REGULAR);
+        return Arrays.asList(movieOne, movieTwo, movieThree);
+    }
+
+    private static Vector<Rental> createRentals(Collection<Movie> movies) {
+        Vector<Rental> rentals = new Vector<>();
+        movies.stream().forEach(m -> rentals.add(new Rental(m, (int) (Math.random() * 10) + 1)));
+        return rentals;
+    }
+}
