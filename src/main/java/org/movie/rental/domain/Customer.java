02036ac2c99b4b050e0789fd1159f5d966ccd124
pawelj <allmightypol@gmail.com>
2018-08-27 17:06:59 +0200

    - extracting getTotalAmount method - moving getTotalAmount method to Rental obj.

--- src/main/java/org/movie/rental/domain/Customer.java
+++ src/main/java/org/movie/rental/domain/Customer.java
@@ -25,36 +25,16 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "org.movie.rental.domain.Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
-
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
+            Rental rental = (Rental) rentals.nextElement();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    rental.getDaysRented() > 1) frequentRenterPoints++;
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getThisAmount()) + "\n";
+            totalAmount += rental.getThisAmount();
 
         }
         //add footer lines
@@ -63,4 +43,5 @@ public class Customer {
                 " frequent renter points";
         return result;
     }
+
 }
